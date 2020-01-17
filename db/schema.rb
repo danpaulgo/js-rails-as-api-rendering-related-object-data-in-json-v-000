3e528271fb84220ca8df28e1b4072ebfcc75af7e
Daniel Goldberg <danpaulgo@aol.com>
2020-01-17 06:12:25 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_17_061054) do
+ActiveRecord::Schema.define(version: 2020_01_17_061214) do
 
   create_table "birds", force: :cascade do |t|
     t.string "name"
     t.string "species"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.float "latitude"
+    t.float "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
