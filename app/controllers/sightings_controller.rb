be1cdabd7c78f7d9b79b4992e7a1061fe4f4ce7b
Daniel Goldberg <danpaulgo@aol.com>
2020-01-17 06:14:13 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,5 +1,10 @@
 class SightingsController < ApplicationController
 
+  def index
+    sightings = Sighting.all
+    render json: sightings
+  end
+
   def show
     sighting = Sighting.find_by(id: params[:id])
     render json: sighting
